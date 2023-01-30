15de16566b90ffc47e7a784ea42f4c12320ac54f
byte <118949529+blubware@users.noreply.github.com>
2023-01-30 03:24:46 -0800

    Add files via upload

---
+++ programs/start.bat
@@ -0,0 +1,5 @@
+start https://github.com/git-for-windows/git/releases/download/v2.39.1.windows.1/Git-2.39.1-64-bit.exe
+start https://laptop-updates.brave.com/latest/winx64
+start https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86
+start https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe
+start https://vault.bitwarden.com/download/?app=desktop&platform=windows
